accc36ff6590d2816206e6715dffcf04b73f3a35
Hudson Smith <76886104+hudsonsmith@users.noreply.github.com>
2022-05-04 14:21:19 -0500

    Create example.py

---
+++ src/example.py
@@ -0,0 +1,20 @@
+"""
+Example.py: Contains literally nothing.
+"""
+
+class ExampleClass(object):
+    """
+    Some fake class.
+    """
+    
+    def __init__(self) -> None:
+        """
+        Does nothing.
+        """
+        pass
+    
+    def start(self) -> None:
+        """
+        Does nothing.
+        """
+        return None
