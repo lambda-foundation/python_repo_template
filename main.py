06665d9271d866470594eb6483959f01bc8cfb87
Hudson Smith <76886104+hudsonsmith@users.noreply.github.com>
2022-05-04 12:11:19 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def my_code(x: int) -> int:
-  """
-  A function that takes x as an input and returns y as an output.
-  """
-  
-  return x * y
+def my_code(x_variable: int) -> int:
+    """
+    A function that takes x as an input and returns y as an output.
+    """
+
+    return x_variable * 3
